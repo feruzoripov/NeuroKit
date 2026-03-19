25882240d1aaf3b061389f2ffcd9cb638200e9eb
Johannes Herforth <johannes@herforth.net>
2026-03-19 19:27:00 +0100

    [Maintenance] Fix formatting

--- neurokit2/data/read_acqknowledge.py
+++ neurokit2/data/read_acqknowledge.py
@@ -8,6 +8,7 @@ import pandas as pd
 from ..misc import NeuroKitWarning
 from ..signal import signal_resample
 
+
 def read_acqknowledge(filename, sampling_rate="max", resample_method="interpolation", impute_missing=True):
     """**Read and format a BIOPAC's AcqKnowledge file into a pandas' dataframe**
 
